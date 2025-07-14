eec02cd90a5638f3db542b6507dbb1007bb1fd6b
Krishil Gandhi <47539334+Vasu30@users.noreply.github.com>
2025-07-14 10:25:10 +0530

    Time: 0 ms (100%), Space: 55.3 MB (54.24%) - LeetHub

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getDecimalValue(head: ListNode | null): number {
+     let num = 0;
+
+    while (head !== null) {
+        num = (num << 1) | head.val;  // Left shift and add the current bit
+        head = head.next;
+    }
+
+    return num;
+};
