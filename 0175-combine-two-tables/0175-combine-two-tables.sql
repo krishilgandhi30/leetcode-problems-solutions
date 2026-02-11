69cf0ce29476e0e36f066a4400d8c4a4a1d33230
Krishil Gandhi <krishilgandhi30@gmail.com>
2026-02-11 10:38:11 +0530

    Time: 261 ms (34.14%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -4,7 +4,6 @@ SELECT
     p.lastName,
     a.city,
     a.state
-FROM 
-    Person p
-LEFT JOIN 
-    Address a ON p.personId = a.personId;
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId;
