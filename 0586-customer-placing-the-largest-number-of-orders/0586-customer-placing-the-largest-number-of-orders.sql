af18418e6dc21e2fad5b113c25006496ebae35ce
Krishil Gandhi <krishilgandhi30@gmail.com>
2026-02-24 11:55:50 +0530

    Time: 235 ms (69.38%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,6 +1,9 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT customer_number
-FROM Orders
-GROUP BY customer_number
-ORDER BY COUNT(order_number) DESC
-LIMIT 1;
+FROM (
+    SELECT customer_number, 
+           RANK() OVER (ORDER BY COUNT(order_number) DESC) as rnk
+    FROM Orders
+    GROUP BY customer_number
+) ranked_customers
+WHERE rnk = 1;
